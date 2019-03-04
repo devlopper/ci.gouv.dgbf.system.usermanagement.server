9ed8bfa82c043e077ecfc442f2399a1923427b43
Christian Yao Komenan <kycdev@gmail.com>
2019-03-04 13:39:31 +0000

    commit

--- usermanagement-server-business-api/src/main/java/ci/gouv/dgbf/system/usermanagement/server/business/api/account/AccountBusiness.java
+++ usermanagement-server-business-api/src/main/java/ci/gouv/dgbf/system/usermanagement/server/business/api/account/AccountBusiness.java
@@ -6,13 +6,5 @@ import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.Ac
 
 public interface AccountBusiness extends BusinessEntity<Account> {
 
-	Account findByCodeByPass(String code,String pass);
-	
-	/**
-	 * Authenticate a code. If code is known from the system then all related data like profile,privilege and so on are loaded
-	 * @param code
-	 * @param pass
-	 * @return
-	 */
-	Account authenticate(String code,String pass);
+
 }
--- usermanagement-server-business-impl/src/test/java/ci/gouv/dgbf/system/usermanagement/server/business/impl/integration/account/AccountFunctionIntegrationTest.java
+++ usermanagement-server-business-impl/src/test/java/ci/gouv/dgbf/system/usermanagement/server/business/impl/integration/account/AccountFunctionIntegrationTest.java
@@ -17,42 +17,11 @@ public class AccountFunctionIntegrationTest extends AbstractBusinessArquillianIn
 		String code = __getRandomCode__();
 		String pass = __getRandomString__();
 		Account account = new Account().setCode(code).setPass(pass);
-		__inject__(TestBusinessCreate.class).addObjects(account).execute();
-	}
-	
-	@Test
-	public void findAccountByCodeByPass() throws Exception{
-		String code = __getRandomCode__();
-		String pass = __getRandomString__();
-		Account account = null;
-		account = __inject__(AccountBusiness.class).findByCodeByPass(code, pass);
-		assertionHelper.assertNull("account with code<<"+code+">> and pass <<"+pass+">> found",account);
-		
-		account = new Account().setCode(code).setPass(pass);
 		__inject__(AccountBusiness.class).create(account);
-				
-		account = __inject__(AccountBusiness.class).findByCodeByPass(code, pass);
-		assertionHelper.assertNotNull("account with code<<"+code+">> and pass <<"+pass+">> not found",account);
+		__inject__(AccountBusiness.class).delete(account);
+		//__inject__(TestBusinessCreate.class).addObjects(account).execute();
 	}
 	
-	@Test
-	public void authenticate_success() throws Exception{
-		String code = __getRandomCode__();
-		String pass = __getRandomString__();
-		Account account = null;
-		
-		account = new Account().setCode(code).setPass(pass);
-		__inject__(AccountBusiness.class).create(account);
-				
-		account = __inject__(AccountBusiness.class).authenticate(code, pass);
-		assertionHelper.assertNotNull("account with code<<"+code+">> and pass <<"+pass+">> not found",account);
-	}
 	
-	@Test(expected=RuntimeException.class)
-	public void authenticate_throwable_code_or_pass_not_found() throws Exception{
-		String code = __getRandomCode__();
-		String pass = __getRandomString__();
-		__inject__(AccountBusiness.class).authenticate(code, pass);
-	}
 	
 }
