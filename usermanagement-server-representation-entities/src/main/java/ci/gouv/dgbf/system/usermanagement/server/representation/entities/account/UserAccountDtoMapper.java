8f9ace052d5d077df50166cc960a134dfdafad91
Christian Yao Komenan <kycdev@gmail.com>
2019-10-03 23:13:15 +0000

    commit

--- usermanagement-server-representation-entities/src/main/java/ci/gouv/dgbf/system/usermanagement/server/representation/entities/account/UserAccountDtoMapper.java
+++ usermanagement-server-representation-entities/src/main/java/ci/gouv/dgbf/system/usermanagement/server/representation/entities/account/UserAccountDtoMapper.java
@@ -1,16 +1,41 @@
 package ci.gouv.dgbf.system.usermanagement.server.representation.entities.account;
 
+import java.util.Collection;
+
 import org.cyk.utility.__kernel__.instance.InstanceHelper;
 import org.cyk.utility.server.representation.AbstractMapperSourceDestinationImpl;
 import org.mapstruct.Mapper;
 
 import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.UserAccount;
 import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Function;
+import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.FunctionScope;
+import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Profile;
+import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Scope;
+import ci.gouv.dgbf.system.usermanagement.server.representation.entities.account.role.FunctionDto;
+import ci.gouv.dgbf.system.usermanagement.server.representation.entities.account.role.FunctionScopeDto;
+import ci.gouv.dgbf.system.usermanagement.server.representation.entities.account.role.ProfileDto;
+import ci.gouv.dgbf.system.usermanagement.server.representation.entities.account.role.ScopeDto;
 
 @Mapper
 public abstract class UserAccountDtoMapper extends AbstractMapperSourceDestinationImpl<UserAccountDto, UserAccount> {
 	private static final long serialVersionUID = 1L;
 
+	public Collection<Function> getFuntions(Collection<FunctionDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(Function.class, dtos);
+    }
+	
+	public Collection<Profile> getProfiles(Collection<ProfileDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(Profile.class, dtos);
+    }
+	
+	public Collection<Scope> getScopes(Collection<ScopeDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(Scope.class, dtos);
+    }
+	
+	public Collection<FunctionScope> getFunctionScopes(Collection<FunctionScopeDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(FunctionScope.class, dtos);
+    }
+	
 	@Override
 	protected void __listenGetSourceAfter__(UserAccount destination, UserAccountDto source) {
 		super.__listenGetSourceAfter__(destination, source);
@@ -21,7 +46,6 @@ public abstract class UserAccountDtoMapper extends AbstractMapperSourceDestinati
 	@Override
 	protected void __listenGetDestinationAfter__(UserAccountDto source, UserAccount destination) {
 		super.__listenGetDestinationAfter__(source, destination);
-		destination.setFunctions(InstanceHelper.getIdentifiedFromIdentifiers(Function.class, destination.getFunctions()));
 		if(destination.getUser() != null)
 			destination.getUser().setFunctions(destination.getFunctions());
 	}
--- usermanagement-server-representation-entities/src/main/java/ci/gouv/dgbf/system/usermanagement/server/representation/entities/account/role/ProfileDtoMapper.java
+++ usermanagement-server-representation-entities/src/main/java/ci/gouv/dgbf/system/usermanagement/server/representation/entities/account/role/ProfileDtoMapper.java
@@ -1,5 +1,7 @@
 package ci.gouv.dgbf.system.usermanagement.server.representation.entities.account.role;
 
+import java.util.Collection;
+
 import org.cyk.utility.__kernel__.instance.InstanceHelper;
 import org.cyk.utility.__kernel__.object.__static__.representation.Action;
 import org.cyk.utility.__kernel__.string.Strings;
@@ -7,11 +9,20 @@ import org.cyk.utility.server.representation.AbstractMapperSourceDestinationImpl
 import org.mapstruct.Mapper;
 
 import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Function;
+import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Privilege;
 import ci.gouv.dgbf.system.usermanagement.server.persistence.entities.account.role.Profile;
 
 @Mapper
 public abstract class ProfileDtoMapper extends AbstractMapperSourceDestinationImpl<ProfileDto, Profile> {
 	private static final long serialVersionUID = 1L;
+	
+	public Collection<Function> getFuntions(Collection<FunctionDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(Function.class, dtos);
+    }
+	
+	public Collection<Privilege> getPrivileges(Collection<PrivilegeDto> dtos) {
+    	return InstanceHelper.getIdentifiedFromIdentifiers(Privilege.class, dtos);
+    }
 	
 	@Override
 	protected Strings __getActionsIdentifiers__(Profile destination, ProfileDto source) {
@@ -19,12 +30,6 @@ public abstract class ProfileDtoMapper extends AbstractMapperSourceDestinationIm
 		if(strings != null)
 			strings.remove(Action.IDENTIFIER_DELETE);
 		return strings; 
-	}
-	
-	@Override
-	protected void __listenGetDestinationAfter__(ProfileDto source, Profile destination) {
-		super.__listenGetDestinationAfter__(source, destination);
-		destination.setFunctions(InstanceHelper.getIdentifiedFromIdentifiers(Function.class, destination.getFunctions()));
 	}
 	
 }
